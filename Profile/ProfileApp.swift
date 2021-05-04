43d74eb2a6e4afdd7f414ec872a91a514ef10a0b
Jevon Charles <jevoncharles@me.com>
2021-05-04 19:21:19 -0400

    Initial Commit

---
+++ Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Jevon Charles on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
